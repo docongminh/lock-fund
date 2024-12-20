3dfbf52d9ff42ad3e45c71037075033244574acc
docongminh <minhdc@192.168.102.71>
2024-12-20 10:10:21 +0700

    supported token2022

--- programs/lock-fund/src/instructions/transfer_token.rs
+++ programs/lock-fund/src/instructions/transfer_token.rs
@@ -1,4 +1,7 @@
-use anchor_spl::token::{Mint, Token, TokenAccount, Transfer};
+use anchor_spl::{
+    token::{TransferChecked, ID},
+    token_interface::{Mint, TokenAccount, TokenInterface, TransferChecked as TransferChecked2022},
+};
 
 use crate::*;
 
@@ -18,7 +21,7 @@ pub struct TransferToken<'info> {
         associated_token::mint = mint_token,
         associated_token::authority = escrow
     )]
-    pub escrow_token: Account<'info, TokenAccount>,
+    pub escrow_token: InterfaceAccount<'info, TokenAccount>,
 
     /// CHECK: Recipient Token Account.
     #[account(
@@ -26,38 +29,59 @@ pub struct TransferToken<'info> {
         associated_token::mint = mint_token,
         associated_token::authority = recipient
     )]
-    pub recipient_token: Account<'info, TokenAccount>,
+    pub recipient_token: InterfaceAccount<'info, TokenAccount>,
 
     /// CHECK: This account use to validate transfer recipient
     pub recipient: AccountInfo<'info>,
 
-    #[account(mut)]
-    pub mint_token: Account<'info, Mint>,
+    #[account(mint::token_program = token_program)]
+    pub mint_token: InterfaceAccount<'info, Mint>,
 
     #[account(mut)]
     pub authority: Signer<'info>,
 
     pub approver: Signer<'info>,
     /// Token program.
-    pub token_program: Program<'info, Token>,
+    pub token_program: Interface<'info, TokenInterface>,
 }
 
 pub fn transfer_token_handler(ctx: Context<TransferToken>, amount: u64) -> Result<()> {
     let config_account = ctx.accounts.config_account.load()?;
     let escrow_seeds = escrow_seeds!(config_account);
+    let token_program = &ctx.accounts.token_program;
 
-    anchor_spl::token::transfer(
-        CpiContext::new_with_signer(
-            ctx.accounts.token_program.to_account_info(),
-            Transfer {
-                from: ctx.accounts.escrow_token.to_account_info(),
-                to: ctx.accounts.recipient_token.to_account_info(),
-                authority: ctx.accounts.escrow.to_account_info(),
-            },
-            &[&escrow_seeds[..]],
-        ),
-        amount,
-    )?;
+    let decimals = ctx.accounts.mint_token.decimals;
+    if token_program.key().eq(&ID.key()) {
+        anchor_spl::token::transfer_checked(
+            CpiContext::new_with_signer(
+                token_program.to_account_info(),
+                TransferChecked {
+                    from: ctx.accounts.escrow_token.to_account_info(),
+                    mint: ctx.accounts.mint_token.to_account_info(),
+                    to: ctx.accounts.recipient_token.to_account_info(),
+                    authority: ctx.accounts.escrow.to_account_info(),
+                },
+                &[&escrow_seeds[..]],
+            ),
+            amount,
+            decimals,
+        )?;
+    } else {
+        anchor_spl::token_2022::transfer_checked(
+            CpiContext::new_with_signer(
+                token_program.to_account_info(),
+                TransferChecked2022 {
+                    from: ctx.accounts.escrow_token.to_account_info(),
+                    mint: ctx.accounts.mint_token.to_account_info(),
+                    to: ctx.accounts.recipient_token.to_account_info(),
+                    authority: ctx.accounts.escrow.to_account_info(),
+                },
+                &[&escrow_seeds[..]],
+            ),
+            amount,
+            decimals,
+        )?;
+    }
 
     emit_cpi!(TransferEvent {
         from: ctx.accounts.escrow_token.key(),
