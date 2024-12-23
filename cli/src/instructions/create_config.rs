e037c568f57372a10f53edc8cec9d021bd496c7d
docongminh <minhdc@192.168.1.213>
2024-12-23 21:37:07 +0700

    cli create config

---
+++ cli/src/instructions/create_config.rs
@@ -0,0 +1,65 @@
+use crate::*;
+
+use anchor_client::{
+    anchor_lang::{InstructionData, ToAccountMetas},
+    solana_sdk::{signature::Keypair, signer::Signer},
+};
+use anchor_lang::solana_program;
+use anchor_spl::token;
+use anyhow::{Ok, Result};
+use solana_rpc_client::rpc_client::RpcClient;
+use solana_sdk::{
+    compute_budget::ComputeBudgetInstruction, instruction::Instruction, pubkey::Pubkey,
+    signature::Signature, transaction::Transaction,
+};
+
+pub struct CreateConfigParams {
+    pub cliff_time_duration: u64,
+    pub amount_per_day: u64,
+    pub update_actor_mode: u8,
+    pub enable_transfer_full: u8,
+    pub recipient: Pubkey,
+    pub approver: Pubkey,
+}
+
+pub fn create_config(
+    client: RpcClient,
+    keypair: Keypair,
+    params: CreateConfigParams,
+) -> Result<Signature> {
+    let (escrow, _bump) = Pubkey::find_program_address(
+        &[lock_fund::ESCROW_SEED, keypair.pubkey().as_ref()],
+        &lock_fund::ID,
+    );
+    let (config_account, _bump) =
+        Pubkey::find_program_address(&[lock_fund::CONFIG_SEED, escrow.as_ref()], &lock_fund::ID);
+
+    let ix = Instruction {
+        program_id: lock_fund::ID,
+        accounts: lock_fund::accounts::CreateConfig {
+            authority: keypairs[0].pubkey(),
+            config_account: config_account,
+            escrow: escrow,
+            recipient: params.recipient,
+            approver: params.approver,
+            system_program: solana_program::system_program::id(),
+        }
+        .to_account_metas(None),
+        data: lock_fund::instruction::CreateConfig {
+            params: lock_fund::CreateConfigParams {
+                cliff_time_duration: params.cliff_time_duration,
+                amount_per_day: params.amount_per_day,
+                update_actor_mode: params.update_actor_mode,
+                enable_transfer_full: params.enable_transfer_full,
+            },
+        }
+        .data(),
+    };
+    let blockhash = client.get_latest_blockhash().unwrap();
+    let tx =
+        Transaction::new_signed_with_payer(&ix, Some(keypair.pubkey()), &[&keypair], blockhash);
+    let sig = client
+        .send_and_confirm_transaction_with_spinner(&transaction)
+        .unwrap();
+    Ok(sig)
+}
---
+++ cli/src/instructions/mod.rs
@@ -0,0 +1,3 @@
+pub mod create_config;
+
+pub use create_config::*;
