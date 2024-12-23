59d6a5ec2e5bc67fbb4c1e92bbf1197a83b36bf5
docongminh <minhdc@minhdcs-MacBook-Pro.local>
2024-12-23 17:53:33 +0700

    add logic encrypt/decrypt cli

--- cli/src/action.rs
+++ cli/src/action.rs
@@ -1,4 +1,10 @@
+use std::{fs, io::BufWriter};
+
 use anyhow::Result;
+use serde_json;
+use sha2::{Sha256, Digest};
+
+use crate::*;
 
 #[derive(Debug)]
 pub enum Action {
@@ -14,16 +20,47 @@ pub enum Action {
     TransferSol {},
 }
 
+pub fn hash_to_32_bytes(input: &str) -> [u8;32] {
+    let mut hasher = Sha256::new();
+    hasher.update(input);
+    hasher.finalize().into()
+}
+
 pub fn handler(action: Action) -> Result<()> {
     match action {
         Action::Encrypt {
             private_key,
             password,
-        } => {}
+        } => {
+            let file = fs::File::open(private_key).expect("file should open read only");
+            let private_key_data: Vec<u8> =
+                serde_json::de::from_reader(file).expect("file should be proper JSON");
+            let to_32_bytes = hash_to_32_bytes(&password);
+            let encrypted_data = cipher::encrypt(private_key_data.as_slice(), &to_32_bytes, None).unwrap();
+            println!("encrypted private key: {:?}", encrypted_data);
+
+            // write encrypted data to output
+            let output = fs::File::create("encrypted.json").expect("Failed to create file output");
+            let writer = BufWriter::new(output);
+            serde_json::to_writer(writer, &encrypted_data)?;
+
+        }
         Action::Decrypt {
             encrypted,
             password,
-        } => {}
+        } => {
+            let file = fs::File::open(encrypted).expect("file should open read only");
+            let encrypted_data: Vec<u8> =
+                serde_json::de::from_reader(file).expect("file should be proper JSON");
+            let to_32_bytes = hash_to_32_bytes(&password);
+            let private_key = cipher::decrypt(&encrypted_data.as_slice(), &to_32_bytes, None).unwrap();
+            println!("private key: {:?}", private_key);
+
+            // write encrypted data to output
+            let output = fs::File::create("decrypted.json").expect("Failed to create file output");
+            let writer = BufWriter::new(output);
+            serde_json::to_writer(writer, &private_key)?;
+        }
         Action::TransferToken {} => {}
         Action::TransferSol {} => {}
     }
--- cli/src/cipher.rs
+++ cli/src/cipher.rs
@@ -4,61 +4,51 @@ use aes_gcm::{
 };
 use anyhow::{anyhow, Result};
 
-pub struct Cipher;
+pub fn encrypt(private_key: &[u8], password: &[u8], aad: Option<&[u8]>) -> Result<Vec<u8>> {
+    let pass_key: &GenericArray<u8, _> = GenericArray::<u8, _>::from_slice(password);
+    let cipher = Aes256Gcm::new(pass_key);
+    let nonce = Aes256Gcm::generate_nonce(&mut OsRng);
 
-impl Cipher {
-    pub fn encrypt(
-        &self,
-        private_key: &[u8],
-        password: &[u8],
-        aad: Option<&[u8]>,
-    ) -> Result<Vec<u8>> {
-        let pass_key: &GenericArray<u8, _> = GenericArray::<u8, _>::from_slice(password);
-        let cipher = Aes256Gcm::new(pass_key);
-        let nonce = Aes256Gcm::generate_nonce(&mut OsRng);
+    let aad_option = match aad {
+        Some(aad) => aad,
+        _ => &[],
+    };
+    let payload = Payload {
+        msg: private_key,
+        aad: aad_option,
+    };
 
-        let aad_option = match aad {
-            Some(aad) => aad,
-            _ => &[],
-        };
-        let payload = Payload {
-            msg: private_key,
-            aad: aad_option,
-        };
+    cipher.encrypt(&nonce, payload).map_or_else(
+        |_| Err(anyhow!("Failed to encrypt data")),
+        |ciphertext| {
+            let mut encrypted_data = nonce.to_vec();
+            encrypted_data.extend_from_slice(&ciphertext);
+            Ok(encrypted_data)
+        },
+    )
+}
 
-        cipher.encrypt(&nonce, payload).map_or_else(
-            |_| Err(anyhow!("Failed to encrypt data")),
-            |ciphertext| {
-                let mut encrypted_data = nonce.to_vec();
-                encrypted_data.extend_from_slice(&ciphertext);
-                Ok(encrypted_data)
-            },
-        )
-    }
+pub fn decrypt(
+    encrypted_private_key: &[u8],
+    password: &[u8],
+    aad: Option<&[u8]>,
+) -> Result<Vec<u8>> {
+    let pass_key = GenericArray::from_slice(password);
+    let cipher = Aes256Gcm::new(pass_key);
+    let nonce = GenericArray::from_slice(&encrypted_private_key[..12]);
+    let ciphertext = &encrypted_private_key[12..];
+    let aad_option = match aad {
+        Some(aad) => aad,
+        _ => &[],
+    };
+    let payload = Payload {
+        msg: ciphertext,
+        aad: aad_option,
+    };
 
-    pub fn decrypt(
-        &self,
-        encrypted_private_key: &[u8],
-        password: &[u8],
-        aad: Option<&[u8]>,
-    ) -> Result<Vec<u8>> {
-        let pass_key = GenericArray::from_slice(password);
-        let cipher = Aes256Gcm::new(pass_key);
-        let nonce = GenericArray::from_slice(&encrypted_private_key[..12]);
-        let ciphertext = &encrypted_private_key[12..];
-        let aad_option = match aad {
-            Some(aad) => aad,
-            _ => &[],
-        };
-        let payload = Payload {
-            msg: ciphertext,
-            aad: aad_option,
-        };
-
-        cipher
-            .decrypt(nonce, payload)
-            .map_or_else(|_| Err(anyhow!("Failed to decrypt data")), Ok)
-    }
+    cipher
+        .decrypt(nonce, payload)
+        .map_or_else(|_| Err(anyhow!("Failed to decrypt data")), Ok)
 }
 
 #[cfg(test)]
@@ -71,13 +61,12 @@ mod tests {
     #[test]
     fn test_with_aad() {
         let keypair = Keypair::new();
-        let cipher = Cipher;
         let private_key = keypair.secret().as_bytes();
 
         let password = [0_u8; 32];
-        let encrypted = cipher.encrypt(private_key, &password, Some(&AAD)).unwrap();
+        let encrypted = encrypt(private_key, &password, Some(&AAD)).unwrap();
 
-        let decrypted = cipher.decrypt(&encrypted, &password, Some(&AAD)).unwrap();
+        let decrypted = decrypt(&encrypted, &password, Some(&AAD)).unwrap();
 
         assert_eq!(private_key.as_ref(), decrypted);
     }
@@ -85,13 +74,12 @@ mod tests {
     #[test]
     fn test_without_aad() {
         let keypair = Keypair::new();
-        let cipher = Cipher;
         let private_key = keypair.secret().as_bytes();
 
         let password = [0_u8; 32];
-        let encrypted = cipher.encrypt(private_key, &password, None).unwrap();
+        let encrypted = encrypt(private_key, &password, None).unwrap();
 
-        let decrypted = cipher.decrypt(&encrypted, &password, None).unwrap();
+        let decrypted = decrypt(&encrypted, &password, None).unwrap();
 
         assert_eq!(private_key.as_ref(), decrypted);
     }
